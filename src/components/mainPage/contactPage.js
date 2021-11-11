9b7f206e2a90209445be97dc53de2079a512e950
Teofanescu Cosmin <cosminteofanescu@gmail.com>
2021-11-11 16:20:45 +0200

    add a small but nice form, next step is to add useState

--- src/components/mainPage/contactPage.js
+++ src/components/mainPage/contactPage.js
@@ -1,7 +1,29 @@
+import { useState } from "react";
+import StyledForm from "../htmlElements/contact/styledForm";
 
 function ContactPage () {
+    const [formData,setFormData] = useState({
+        name:"",
+        email:"",
+        phoneNumber:"",
+    })
     return (
-        <h1> Hello from contactPage.js</h1>
+        <StyledForm>
+            <section>
+                <label>Name</label>
+                <input></input>
+            </section>
+            <section>
+                <label>Email</label>
+                <input></input>
+            </section>
+            <section>
+                <label>Phone Number</label>
+                <input></input>
+            </section>
+            <textarea />
+            <button>Send Message</button>
+        </StyledForm>
     );
 };
 
