c4a324feaea057c37092ead295f488eafe3efb4a
Teofanescu Cosmin <cosminteofanescu@gmail.com>
2021-11-13 19:16:29 +0200

    finish first stage of the game

--- src/components/videoPoker/cardGameContainer.js
+++ src/components/videoPoker/cardGameContainer.js
@@ -3,11 +3,16 @@ import styled from "styled-components";
 const StyledCardGameContainer = styled.div`
 display:flex;
 justify-content:center;
+margin-top:1em;
 section {
     margin: 0 1em;
     p {
         text-align:center;
         margin:0.5em 0;
+    }
+    img {
+        width:9em;
+        height:12em;
     }
 }
 `;
--- src/components/videoPoker/videoPoker.js
+++ src/components/videoPoker/videoPoker.js
@@ -25,36 +25,80 @@ const tableInfo ={
 }
 
 function VideoPoker () {
-    const [card1,setCard1] = useState({name:"",link:"",value:"",type:"",keep:true});
+    const [card1,setCard1] = useState({name:"",link:"",value:"",type:"",keep:false});
     const [card2,setCard2] = useState({name:"",link:"",value:"",type:"",keep:false});
-    const [card3,setCard3] = useState({name:"",link:"",value:"",type:"",keep:true});
+    const [card3,setCard3] = useState({name:"",link:"",value:"",type:"",keep:false});
     const [card4,setCard4] = useState({name:"",link:"",value:"",type:"",keep:false});
-    const [card5,setCard5] = useState({name:"",link:"",value:"",type:"",keep:true});
+    const [card5,setCard5] = useState({name:"",link:"",value:"",type:"",keep:false});
     
     const[poker,setPoker]=useState({
+        gameStage:0,
+        btnText:"Start New Game",
         bet:1,
         score:100,
         winCounter:0,
         loseCounter:0,
         gameText:"Select Your Bet And Click The Start Game Button !",
     });
-    const {bet , score , winCounter , loseCounter,gameText} = poker;
+    const {bet , score , winCounter , loseCounter,gameText , gameStage , btnText} = poker;
 
         function onBetChange (event) {
             const btnType = event.target.name;
+            if(gameStage === 0){
             setPoker((prevState)=>({
                 ...prevState,
                 bet: btnType === "increment" && bet <score ? bet +1 :
                 btnType === "decrement" && bet > 1 ? bet -1 : bet,
-            })) 
+            })) }
         }
-        function testFirst (event){
+        function holdCardOnClick (event){
             const card =event.target.name;
+            if(gameStage === 1){
             if(card === 'card1') {card1.keep === true ? setCard1((prevState)=> ({...prevState,keep:false,})) : setCard1((prevState)=> ({...prevState,keep:true,}))}
             if(card === 'card2') {card2.keep === true ? setCard2((prevState)=> ({...prevState,keep:false,})) : setCard2((prevState)=> ({...prevState,keep:true,}))}
             if(card === 'card3') {card3.keep === true ? setCard3((prevState)=> ({...prevState,keep:false,})) : setCard3((prevState)=> ({...prevState,keep:true,}))}
             if(card === 'card4') {card4.keep === true ? setCard4((prevState)=> ({...prevState,keep:false,})) : setCard4((prevState)=> ({...prevState,keep:true,}))}
             if(card === 'card5') {card5.keep === true ? setCard5((prevState)=> ({...prevState,keep:false,})) : setCard5((prevState)=> ({...prevState,keep:true,}))}
+        }}
+        function gameButton (event) {
+            if(gameStage === 0) {
+                exchangeCards();
+                setPoker((prevState)=>({
+                    ...prevState,
+                    gameStage:1,
+                    score:prevState.score - prevState.bet,
+                    btnText:"Exchange Cards",
+                    gameText:"Click on card you wanna take next stage ! The other will swap with a random card. Click 'Exchange Cards' to continue .",
+                }))
+            }
+
+        }
+        function exchangeCards (){
+            if(card1.keep === false) {
+                const index = Math.floor(Math.random()*gameDeck.length);
+                const newCard = gameDeck.splice(index,1);
+                setCard1(...newCard);
+            }
+            if(card2.keep === false) {
+                const index = Math.floor(Math.random()*gameDeck.length);
+                const newCard = gameDeck.splice(index,1);
+                setCard2(...newCard);
+            }
+            if(card3.keep === false) {
+                const index = Math.floor(Math.random()*gameDeck.length);
+                const newCard = gameDeck.splice(index,1);
+                setCard3(...newCard);
+            }
+            if(card4.keep === false) {
+                const index = Math.floor(Math.random()*gameDeck.length);
+                const newCard = gameDeck.splice(index,1);
+                setCard4(...newCard);
+            }
+            if(card5.keep === false) {
+                const index = Math.floor(Math.random()*gameDeck.length);
+                const newCard = gameDeck.splice(index,1);
+                setCard5(...newCard);
+            }
         }
     return (
 <>
@@ -82,7 +126,7 @@ function VideoPoker () {
                     <p>{bet}</p>
                     <button name="increment" onClick={onBetChange}>Bet +1</button>
                 </section>
-            <button>Start New Game</button>
+            <button onClick={gameButton}>{btnText}</button>
             <p>Points : {score}</p>
         </StyledGameProgressContainer>
         <PokerText>{gameText}</PokerText>
@@ -93,35 +137,35 @@ function VideoPoker () {
                 link={ card1.link.length === 0 ? noCard : card1.link}
                 altName={card1.name.length === 0 ? 'Back Of a Card Deck' : card1.name}
                 hold={card1.keep}
-                holdEvent={testFirst}
+                holdEvent={holdCardOnClick}
             />
              <CardGame 
                 name='card2'
                 link={ card2.link.length === 0 ? noCard : card2.link}
                 altName={card2.name.length === 0 ? 'Back Of a Card Deck' : card2.name}
                 hold={card2.keep}
-                holdEvent={testFirst}
+                holdEvent={holdCardOnClick}
             />
              <CardGame 
                 name='card3'
                 link={ card3.link.length === 0 ? noCard : card3.link}
                 altName={card3.name.length === 0 ? 'Back Of a Card Deck' : card3.name}
                 hold={card3.keep}
-                holdEvent={testFirst}
+                holdEvent={holdCardOnClick}
             />
              <CardGame 
                 name='card4'
                 link={ card4.link.length === 0 ? noCard : card4.link}
                 altName={card4.name.length === 0 ? 'Back Of a Card Deck' : card4.name}
                 hold={card4.keep}
-                holdEvent={testFirst}
+                holdEvent={holdCardOnClick}
             />
              <CardGame 
                 name='card5'
                 link={ card5.link.length === 0 ? noCard : card5.link}
                 altName={card5.name.length === 0 ? 'Back Of a Card Deck' : card5.name}
                 hold={card5.keep}
-                holdEvent={testFirst}
+                holdEvent={holdCardOnClick}
             />
         </StyledCardGameContainer>
         
