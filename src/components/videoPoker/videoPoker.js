7b1957fe347a5b37534ec1f78980e4baff4e428b
Teofanescu Cosmin <cosminteofanescu@gmail.com>
2021-11-13 04:09:19 +0200

    add bet button , start game and display of score

--- src/components/videoPoker/videoPoker.js
+++ src/components/videoPoker/videoPoker.js
@@ -18,13 +18,30 @@ function VideoPoker () {
         twopair:1,
     }
     const[poker,setPoker]=useState({
-        bet:3,
+        bet:1,
         score:100,
         winCounter:0,
         loseCounter:0,
         gameText:"Select Your Bet And Click The Start Game Button !",
     });
     const {bet , score , winCounter , loseCounter,gameText} = poker;
+
+    function incrementBet (event) {
+        const value = bet+1;
+        if(bet < score){
+        setPoker((prevState)=>({
+            ...prevState,
+            [event.target.name]:value,
+        }))}
+    }
+    function decrementBet (event) {
+        const value = bet-1;
+        if(bet > 1){
+        setPoker((prevState)=>({
+            ...prevState,
+            [event.target.name]:value,
+        }))}
+    }
     return (
 <>
         <div style={{display: 'grid', gridTemplateColumns:'1fr 1fr'}} >
@@ -44,6 +61,17 @@ function VideoPoker () {
                 <p> To add this section soon...</p> 
             </section>
         </HistoryGameContainer>
+        </div>
+        <div className="action-container">
+            <div className="btn-container">
+                <section>
+                    <button name="bet" onClick={decrementBet}>Bet -1</button>
+                    <p>{bet}</p>
+                    <button name="bet" onClick={incrementBet}>Bet +1</button>
+                </section>
+                <button>Start New Game</button>
+                <p>{score}</p>
+            </div>
         </div>
         <PokerText>{gameText}</PokerText>
 </>
